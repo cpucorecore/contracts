7effba0055fc2db288414155e7ab4fd6205d8c40
cpucorecore <cpucorecore@gmail.com>
2020-03-04 20:46:02 +0800

    update ipal

--- solidity/ipal/ipal.sol
+++ solidity/ipal/ipal.sol
@@ -1,38 +1,86 @@
 pragma solidity 0.6.0;
+// using SafeMath for uint256;
 
 contract Ipal {
-    uint16 constant MAX_IPALS = 1024;
+    // using SafeMath for uint256;
     
+    /////////////////////////////////////////// IPAL /////////////////////////////////////////// 
     address adminAddress    = 0xdCad3a6d3569DF655070DEd06cb7A1b2Ccd1D3AF;
-    uint64 minBond          = 1000000000;
+    uint64 minBond          = 1000000;
+    
+    enum UnApproveReason {
+        NONE,
+        BOND_NOT_ENOUGH,
+        UNCLAIM
+    }
     
     struct IpalItem {
         string ipalDeclaration;
         uint256 bond;
         bool isApproved;
+        UnApproveReason unApproveReason;
     }
     
     mapping(address=>IpalItem) public ipals;
-    address[] public keys = new address[](MAX_IPALS);
+    address[] public ipalKeys;
     
     function ipalClaim(string memory ipalDeclaration) public payable {
+        IpalItem memory v;
         IpalItem memory existItem = ipals[msg.sender];
-        if (0 == existItem.bond) {
-            IpalItem memory v;
-            v.ipalDeclaration = ipalDeclaration;
-            v.bond = msg.value;
-            v.isApproved = false;
-            
-            ipals[msg.sender] = v;
-            
-            keys.push(msg.sender);
-        } else {
-            
-        }
+        
+        uint256 targetBond = existItem.bond + msg.value; //TODO CHECK overflow
+        require (targetBond >= minBond);
+        
+        v.ipalDeclaration = ipalDeclaration;
+        v.bond = targetBond;
+        v.isApproved = false;
+        v.unApproveReason = UnApproveReason.NONE;
+        
+        ipals[msg.sender] = v;
+        ipalKeys.push(msg.sender);
+    }
+    
+    function ipalUnClaim() public {
+        IpalItem memory ipal = ipals[msg.sender];
+        
+        require(ipal.bond != 0);
+        
+        msg.sender.transfer(ipal.bond);
+        
+        ipal.bond = 0;
+        ipal.isApproved = false;
+        ipal.unApproveReason = UnApproveReason.UNCLAIM;
+        
+        ipals[msg.sender] = ipal;
+    }
+    
+    function ipalApprove(address addr) public {
+        assert (msg.sender == adminAddress);
+        
+        IpalItem memory ipal = ipals[addr];
+        require (ipal.bond >= minBond);
+        
+        ipal.isApproved = true;
+        ipals[addr] = ipal;
+    }
+    
+    function ipalUnApprove(address addr, UnApproveReason reason) public {
+        assert (msg.sender == adminAddress);
+        
+        IpalItem memory ipal = ipals[addr];
+        require (ipal.bond > 0);
+        
+        ipal.isApproved = false;
+        ipal.unApproveReason = reason;
+        ipals[addr] = ipal;
     }
     
     function getIpalKeys() public view returns(address[] memory v) {
-        return keys;
+        return ipalKeys;
     }
+    
+    
+    /////////////////////////////////////////// CIPAL ///////////////////////////////////////////
 }
+
 
